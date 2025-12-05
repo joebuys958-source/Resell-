0b728bdb3980f7b2753e8d3ad4891d61a976a023
joebuys958-source <joebuys958@gmail.com>
2025-12-05 16:29:53 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-const inventory = JSON.parse(localStorage.getItem("inventory")) || [];
-const sold = JSON.parse(localStorage.getItem("soldItems")) || [];
-
-document.getElementById("total-items").innerText = inventory.length;
-document.getElementById("total-sold").innerText = sold.length;
-
-let profit = 0;
-sold.forEach(i => profit += Number(i.salePrice) - Number(i.purchasePrice));
-document.getElementById("total-profit").innerText = "£" + profit;
+console.log("App loaded");
