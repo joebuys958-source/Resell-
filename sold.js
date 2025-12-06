ed1795af7e83b82b496e037c5a40436edfed3216
joebuys958-source <joebuys958@gmail.com>
2025-12-06 21:40:53 +0000

    Delete sold.js

--- sold.js
+++
@@ -1,9 +0,0 @@
-let sold = JSON.parse(localStorage.getItem("sold")) || [];
-
-const list = document.getElementById("sold-list");
-
-sold.forEach(item => {
-  const li = document.createElement("li");
-  li.textContent = `${item.name} — Sold for £${item.price}`;
-  list.appendChild(li);
-});
