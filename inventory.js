2d4f125ce9c659f5c6033a36d54359155c36bd2e
joebuys958-source <joebuys958@gmail.com>
2025-12-06 21:40:41 +0000

    Delete inventory.js

--- inventory.js
+++
@@ -1,9 +0,0 @@
-let inventory = JSON.parse(localStorage.getItem("inventory")) || [];
-
-const list = document.getElementById("inventory-list");
-
-inventory.forEach(item => {
-  const li = document.createElement("li");
-  li.textContent = `${item.name} — £${item.price}`;
-  list.appendChild(li);
-});
